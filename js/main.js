1b6f1700d819a48be705d61e4586a03a861d7e90
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2019-02-14 15:57:31 +0100

    Different options added
    Clear the board button.
    Save option: Create a URL with the draw that has been drawn.
    Rubber added

--- js/main.js
+++ js/main.js
@@ -4,65 +4,16 @@
  */
 var mouse = { x: 0, y: 0 };
 var mouseDown = false;
+var canvas, ctx, paintElement;
 
 $(window).resize(function () {
     console.log("resize");
-    $("#paint").css({ "width": "100%", "height": "100%", "color": "blue" });
+    $("#paint").css({ "width": "100%", "height": "100%" });
 });
 
 $(() => {
 
     startPaint();
-
-});
-
-function startPaint() {
-    setDefaultValues();
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
-    var paintElement = document.getElementById('paint');
-
-    canvas.width = parseInt(getComputedStyle(paintElement).getPropertyValue('width'));
-    canvas.height = parseInt(getComputedStyle(paintElement).getPropertyValue('height'));
-
-    canvas.style.cssText = "border: 1px solid black";
-    $("#myCanvas").on("mousemove", function (e) {
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-    })
-
-    //Draw a dot
-    $("#myCanvas").on("click", function () {
-        ctx.moveTo(mouse.x, mouse.y);
-        onPaint();
-    })
-
-    /*$("#myCanvas").on("mouseleave", function () {
-        canvas.removeEventListener('mousemove', onPaint, false);
-    });*/
-
-    canvas.addEventListener('mousedown', function () {
-        ctx.beginPath();
-        ctx.moveTo(mouse.x, mouse.y);
-        canvas.addEventListener('mousemove', onPaint, false);
-    }, false)
-
-    document.addEventListener('mouseup', function () {
-        console.log("Mouseup");
-        canvas.removeEventListener('mousemove', onPaint, false);
-    }, false);
-
-    function onPaint() {
-        ctx.lineTo(mouse.x, mouse.y);
-        ctx.stroke();
-    };
-
-    ctx.lineWidth = document.getElementById('lineWidth').value;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = document.getElementById('color').value;
-    ctx.globalAlpha = 1;
-
     $("#color").on('change', function () {
         console.log($("#color").val());
         ctx.strokeStyle = $("#color").val();
@@ -87,16 +38,99 @@ function startPaint() {
         console.log("type selected");
         $("#menu-type .btn").removeClass('active');
         $(this).toggleClass('active');
-        if (this.value === "Pencil")
-            ctx.globalAlpha = 0.03;
-        else if (this.value === "Pen")
-            ctx.globalAlpha = 1;
+        switch (this.value) {
+            case "Pencil":
+                ctx.globalAlpha = 0.03;
+                ctx.strokeStyle = $("#color").val();
+                break;
+            case "Pen":
+                ctx.globalAlpha = 1;
+                ctx.strokeStyle = $("#color").val();
+                break;
+            case "Rubber":
+                console.log(canvas.style.backgroundColor);
+                ctx.strokeStyle = "#ffffff";
+                ctx.globalAlpha = 1;
+                console.log(ctx.strokeStyle);
+                break;
+        }
     })
+
+    $("#myCanvas").on("mousemove", function (e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    })
+
+    //Draw a dot
+    $("#myCanvas").on("click", function () {
+        ctx.moveTo(mouse.x, mouse.y);
+        onPaint();
+    })
+});
+
+function saveCanvas() {
+    console.log(canvas.toDataURL());
+    $("#urlCanvas").val(canvas.toDataURL());
+
+    var copyText = document.getElementById("urlCanvas");
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("The drawing has been saved.\nURL copied to the clipboard")
+}
+
+function loadCanvas() {
+    
+}
+
+function clearCanvas() {
+    startPaint();
+}
+
+function onPaint() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
+
+function startPaint() {
+    setDefaultValues();
+    canvas = document.getElementById('myCanvas');
+    console.log(canvas);
+    ctx = canvas.getContext('2d');
+    paintElement = document.getElementById('paint');
+
+    canvas.width = parseInt(getComputedStyle(paintElement).getPropertyValue('width'));
+    canvas.height = parseInt(getComputedStyle(paintElement).getPropertyValue('height'));
+
+    canvas.style.cssText = "border: 1px solid black";
+
+    canvas.addEventListener('mousedown', function () {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false)
+
+    document.addEventListener('mouseup', function () {
+        console.log("Mouseup");
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    ctx.lineWidth = document.getElementById('lineWidth').value;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = document.getElementById('color').value;
+    ctx.globalAlpha = 1;
 }
 
 function setDefaultValues() {
     $("#lineWidth").val("2");
     $("#color").val("#FFFFF")
     $("#textInput").val($("#lineWidth").val());
+    $("#menu-type .btn").removeClass('active');
     $("#pen").addClass("active")
 }
